c191498e734aad59acaa02ce392d2b92cc20f84a
William Lin <wlin.1094@gmail.com>
2023-10-12 09:40:59 -0400

    Delete Old directory

--- Old/interesting things/snacc.py
+++
@@ -1,3 +0,0 @@
-print('\n'.join([''.join([('YouASnacc'[(x-y) % len
-('YouASnacc')]if((x*0.05)**2+(y*0.1)**2-1)**3-(x*0.05)**2*(y*0.1)**3 <= 0 else ' ' )for
-x in range(-25,30)])for y in range(30,-30, -1)]))
